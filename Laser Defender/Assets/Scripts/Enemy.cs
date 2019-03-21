238e86d5c6ae5142d4c422f926e12c1d6ccb61ed
BeyondTheEcho <steliosklapsis@outlook.com>
2019-03-20 22:35:57 -0400

    Added functionality and comments
    - Added damage functionality and the ability to manage damage
    - Enemies now have health and can be destroyed
    - The players laser now deals damage based on collisions
    - Added comments in an effort to improve readability

---
+++ Laser Defender/Assets/Scripts/Enemy.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    //Config
+    [SerializeField] float health = 100;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // Is triggered on collisions
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        //Assigns the damagecontroller var to the DamageController.cs attached to the object that collides
+        DamageController damagecontroller = collision.gameObject.GetComponent<DamageController>();
+
+        //Manages the damage calculations
+        ManageDamage(damagecontroller);
+    }
+
+    private void ManageDamage(DamageController damagecontroller)
+    {
+        // Applies damage to the health of this object
+        health -= damagecontroller.GetDamage();
+
+        //Destroys the object when the health is <= 0
+        if (health <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
