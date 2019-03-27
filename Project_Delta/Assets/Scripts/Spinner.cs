fb1fffb657639c8c543b69c0d0e9f0591a3ce140
BeyondTheEcho <steliosklapsis@outlook.com>
2019-03-26 21:57:31 -0400

    - Added New Paths - Added New Waves - Added Spinner.cs which causes anything it is attached to, to spin - Made Enemy Bombs Spin

---
+++ Project_Delta/Assets/Scripts/Spinner.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spinner : MonoBehaviour
+{
+    [SerializeField] float spinSpeed = 1000f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Spin();
+    }
+
+    private void Spin()
+    {
+        transform.Rotate(0, 0, spinSpeed * Time.deltaTime);
+    }
+}
