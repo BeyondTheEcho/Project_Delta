85249d364278b3974c0de63a8d58282dfde02902
Stelios Klapsis <steliosklapsis@outlook.com>
2019-03-28 03:11:43 -0400

    Pause Feature / Upgrades Feature Template
    - Added a pause button to the game canvas that effectively freezes world time
    - Added an upgrade menu canvas triggered by the pause game button
    - Added a template upgrade with working buttons, score check and other minor functions

---
+++ Project_Delta/Assets/Scripts/UIManagement.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class UIManagement : MonoBehaviour
+{
+    [Header("Game Status")]
+    [SerializeField] bool isPaused = false;
+    [SerializeField] GameObject upgradeCanvas;
+    [SerializeField] string purchasedText = "Purchased";
+
+    [Header("Double Lasers")]
+    [SerializeField] GameObject doubleLasersText;
+    [SerializeField] bool doubleLasers = false;
+    [SerializeField] int doubleLasersCost = 1;
+
+    //Stored References
+    GameSession gameSession;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        FindGameSession();
+    }
+
+    private void FindGameSession()
+    {
+        gameSession = FindObjectOfType<GameSession>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Pause()
+    {
+        isPaused = !isPaused;
+
+        if (isPaused == true)
+        {        
+            Time.timeScale = .0000001f;
+            upgradeCanvas.SetActive(true);
+        }
+        else if (isPaused == false)
+        {
+            Time.timeScale = 1.0f;
+            upgradeCanvas.SetActive(false);
+        }
+    }
+
+    public void DoubleLasers()
+    {
+        if (doubleLasers == false)
+        {
+            if(gameSession.GetScore() >= doubleLasersCost)
+            {
+                doubleLasers = true;
+                doubleLasersText.GetComponent<TMP_Text>().color = Color.green;
+                doubleLasersText.GetComponent<TMP_Text>().text = purchasedText;
+            }
+            else
+            {
+                //Play Error Sound
+            }
+        }
+    }
+}
